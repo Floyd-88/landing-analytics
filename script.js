f6e8bd3106569d9f60d8934acfe1d789129f530f
Dio Eracler <dioeracler1988@gmail.com>
2024-09-10 23:51:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto', // Shows multiple slides at a time
+    spaceBetween: 20, // Space between slides
+    loop: true, // Enable infinite loop
+    centeredSlides: true, // Center slides in the viewport
+    initialSlide: 0, // Start from the second slide (index is 0-based)
+    autoplay: false, // Disable autoplay
+    });
